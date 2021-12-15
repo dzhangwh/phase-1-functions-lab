877dfb1e9ad8bc4fd3d36c94209eebfce80c0cc6
Daniel Zhang <dzhangwh@gmail.com>
2021-12-15 09:54:55 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks) {  
+return Math.abs(distanceFromHqInBlocks(blocks) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+return Math.abs((start - destination) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+if ((start - destination) > 2500) {
+    return 'cannot travel that far';
+}
+
+
+}
+    
+
+
+
+
+
+
+
