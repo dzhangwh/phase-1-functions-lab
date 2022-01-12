0129a64f7235b999dcee864c90f9ee1fd3e3893c
Daniel Zhang <dzhangwh@gmail.com>
2022-01-12 13:40:14 -0500

    last part

--- index.js
+++ index.js
@@ -11,13 +11,28 @@ function distanceTravelledInFeet(start, destination) {
 return Math.abs((start - destination) * 264);
 }
 
-function calculatesFarePrice(start, destination) {
-if ((start - destination) > 2500) {
-    return 'cannot travel that far';
+const calculatesFarePrice = function(start, destination) {
+    let freeSample = start - destination
+    if (freeSample === 0) {
+        console.log ('test')
+        return 'gives customers a free sample'
+    }
+    
 }
 
+let test = console.log ('test2')
 
-}
+    
+
+    
+    //if ((start - destination) > 2500) {
+    //let tooFar = 'cannot travel that far'
+   // return tooFar
+//}
+//else if (())
+
+
+
     
 
 
